8169096fd4c47f2687144832909b3513fb0696bd
Jack <jack@robisonservice.com>
2016-02-15 13:13:08 -0500

    initial commit

---
+++ update_lbrynet_package.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as sudo"
+  exit
+fi
+
+lbrynet_directory="/Users/${SUDO_USER}/Library/Application Support/lbrynet"
+
+current_version=$(git ls-remote https://github.com/lbryio/lbry.git | grep HEAD | cut -f 1)
+
+if [ -d "$lbrynet_directory" ]; then
+	if [ -f "${lbrynet_directory}/version.txt" ]; then
+		if grep -Fxq "$current_version" "${lbrynet_directory}/version.txt"; then
+			echo "LBRYnet version $current_version is up to date"
+			exit
+		fi
+	fi
+fi
+
+if which stop-lbrynet-daemon &>/dev/null; then
+	daemon_stat=$(stop-lbrynet-daemon)
+fi
+
+tmp=$(mktemp -d)
+cd $tmp
+
+echo "Downloading newest version of lbrynet"
+
+git clone https://github.com/lbryio/lbry.git &>/dev/null
+cd lbry
+
+version=$(git rev-parse HEAD)
+echo "Installing lbrynet"
+sudo python setup.py install &>/dev/null
+mkdir -p "$lbrynet_directory"
+echo $version > "${lbrynet_directory}/version.txt"
+
+echo "Cleaning up"
+
+cd ../../
+sudo rm -rf $tmp
+
+if [[ $daemon_stat = "LBRYnet daemon stopped" ]] &>/dev/null; then
+	sudo lbrynet-daemon
+fi
